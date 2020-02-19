be562a9cfaef1ac9384914e18d6a3dad51cad72d
Théo Boschetti <theoboschetti@mbp-de-theo.lille.fr.ibm.com>
2020-02-19 16:58:31 +0100

    Create SideBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./component/Header";
+import SideBar from "./component/SideBar";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SideBar />
     </div>
   );
 }
