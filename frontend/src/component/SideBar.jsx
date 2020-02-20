343ce728744afa1d2d463075d9f92d87a8f15333
Théo Boschetti <theoboschetti@mbp-de-theo.lille.fr.ibm.com>
2020-02-20 10:52:48 +0100

    SideBar component done

--- frontend/src/component/SideBar.jsx
+++ frontend/src/component/SideBar.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Icon } from "antd";
 import "./SideBar.scss";
 
 export default function SideBar() {
-  return <div className="sideBar"></div>;
+  return (
+    <div className="sideBar">
+      <div className="sideBarContainer">
+        <Icon className="mainIcon" type="fund" />
+        <div>
+          <Icon type="message" />
+          <p>Request a feedback</p>
+          <Icon type="profile" />
+          <p>Your feedback</p>
+        </div>
+      </div>
+    </div>
+  );
 }
